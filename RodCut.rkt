58e4b51504c5cb666fb8f59fded88ee92f464944
Zachary Nelson <31941112+Primeeight@users.noreply.github.com>
2024-03-29 16:57:52 -0500

    Add files via upload

---
+++ RodCut.rkt
@@ -0,0 +1,19 @@
+#lang slideshow
+;Zachary Nelson
+;Ethan Bylek
+
+
+
+
+
+
+
+
+
+
+;Output maximal revenue
+
+;Output optimal length
+;optional length is represented by vecotr of sequences of rectangles
+;Each rectangle is has a 1-inch length.
+;May also want running time.
