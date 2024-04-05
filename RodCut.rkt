c800090a05e064a1c44484c196fe69427d16cd0f
Ethan Bilyk <95322579+EthanBilyk@users.noreply.github.com>
2024-04-05 12:17:52 -0400

    Update RodCut.rkt
    Spelled my name correctly and added a test method that uses the rodCut method. Had to change a few things in the rodCut method for it to work on n>10 values.

--- RodCut.rkt
+++ RodCut.rkt
@@ -1,26 +1,59 @@
 #lang racket
 ;Zachary Nelson
-;Ethan Bylek
+;Ethan Bilyk
 
 ;initial rod v
 ;length n
 (define (rodCut v n)
-  (if (equal? n 0)0
-      {let*
-      [(r -1)]
+  (if (zero? n)
+      0
+      {let
+       ([r -1])
         ;iterate through potential max revenues
-        [for ([i n])
+        [for ([i (in-range 1 (add1 n))])
+          (if (> i 10)
+              (set! i 1)
           ;check each recursive call
-          [set! r (max r (+ (vector-ref v (+ i 1)) (rodCut v (- n (+ i 1)))))
-          ]
-        ]
+          [set! r (max r (+ (vector-ref v i) (rodCut v (- n i))))
+          ])
+         ]
         ;outputed revenue
         r}))
-;test function
-(define (testing)
-  (rodCut(make-vector 3 3)2)
-)
 
+
+; test function
+; this fuction takes a long time to run
+; so use (testing) at your own risk
+(define (testing)
+  ; define prices array
+  (define prices #(0 1 5 8 9 10 17 17 20 24 30))
+
+  ; define test cases
+  (define tests
+    `(("Test case 1: Length 10" ,(rodCut prices 10) 30)
+      ("Test case 2: Length 20" ,(rodCut prices 20) 60)
+      ("Test case 3: Length 30" ,(rodCut prices 30) 90)
+      ("Test case 4: Length 40" ,(rodCut prices 40) 120)
+      ("Test case 5: Length 50" ,(rodCut prices 50) 150)
+      ))
+
+  ; for each test case, run the test
+  (for-each
+   (lambda (test)
+     (let ((name (car test))
+           (result (cadr test))
+           (expected (caddr test)))
+       (display name)
+       ; if it passes the test, display "passed"
+       (if (= result expected)
+           (displayln ": Passed")
+           (begin
+             ; else, display "failed" along with the incorrect result
+             (display ": Failed, expected ")
+             (display expected)
+             (display " but got ")
+             (displayln result)))))
+   tests))
 
 
 
