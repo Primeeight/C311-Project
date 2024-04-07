f5d003b4d4a7cf8ecfc8155dcf44f8150eabecba
zacharywray <zacharywray@gmail.com>
2024-04-07 16:47:07 -0400

    updated the dynamic implementation, removed bugs with lengths of 10 or less.

--- DynamicRodCut.rkt
+++ DynamicRodCut.rkt
@@ -17,28 +17,30 @@
 
 (define (rodCutAux v n v2)
   (define r 0)
-  (if (>= (vector-ref v2 n) 0)
-      (vector-ref v2 n)
+  (if (> (vector-ref v2 n) -1)
+      (set! r (vector-ref v2 n))
       ;like if but we're skipping the else case.      
       (when (> n 0)
         (set! r -1)
         ;iterate through potential max revenues
-          [for ([i (in-range 1 (min n 10))])
+          [for ([i (in-range 0 (min n 10))])
           ;check each recursive call
-          [set! r (max r (+ (vector-ref v (add1 i))(rodCutAux v (- n (add1 i)) v2)))]
+            (define sum (+ (vector-ref v (add1 i))(rodCutAux v (- n (add1 i)) v2)))
+          [set! r (max r sum)]
           ]    
       )
   )
   ;save result, key part of dynammic programming.
-  (vector-set! v2 n r)
+  (when (> r (vector-ref v2 n))
+    (vector-set! v2 n r)
+  )
   ;output revenue.
   r
 )
 
 
 ; test function
-; this function takes a long time to run
-; so use (testing) at your own risk
+; this function should be much faster than Naïve version.
 (define (testing)
   ; define prices array
   (define prices #(0 1 5 8 9 10 17 17 20 24 30))
@@ -46,12 +48,14 @@
   ; define test cases
   (define tests
     `(
+      ("Test case 1: Length 1" ,(rodCut prices 1) 1)
+      ("Test case 2: Length 5" ,(rodCut prices 5) 13)
       ("Test case 1: Length 10" ,(rodCut prices 10) 30)
       ("Test case 2: Length 11" ,(rodCut prices 11) 31)
-      ;("Test case 2: Length 20" ,(rodCut prices 20) 60)
-      ;("Test case 3: Length 30" ,(rodCut prices 30) 90)
-      ;("Test case 4: Length 40" ,(rodCut prices 40) 120)
-      ;("Test case 5: Length 50" ,(rodCut prices 50) 150)
+      ("Test case 2: Length 20" ,(rodCut prices 20) 60)
+      ("Test case 3: Length 30" ,(rodCut prices 30) 90)
+      ("Test case 4: Length 40" ,(rodCut prices 40) 120)
+      ("Test case 5: Length 50" ,(rodCut prices 50) 150)
       ))
 
   ; for each test case, run the test
